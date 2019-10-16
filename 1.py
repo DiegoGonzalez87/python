1d74e3f68fd3b476ee2aced91664cef4cdf4de7a
alejandro <alejhandro0320@gmail.com>
2019-10-15 19:59:35 -0500

    Version

---
+++ 1.py
@@ -0,0 +1,19 @@
+test1= "Colombia"
+test2= "Argentina"
+
+#print(test1)
+lista = [test2, test1, "Oso"]
+
+#print(lista[1], "vs", lista[0])
+
+def test():
+    return test1
+
+print(test())
+
+
+tupla1 = ("Python", "Django")
+
+#print(tupla1[1])
+
+
---
+++ 2.py
@@ -0,0 +1,18 @@
+#holaaaa
+"""
+Que paso, mi don.
+"""
+print ("Soy l pronive")
+
+print(1+9,.2,3)
+
+print("false")
+
+str("test") #Convertir Datos
+
+["Strint", 1, [1,2,3], "true"] #Listas
+
+{"name": "Emilio"} #Diccionarios
+
+("Python", "Django") #tuplas
+
